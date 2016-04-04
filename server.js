431bd7fa354f3147e3215933f74ca14a4bdb3ff0
Olivier Coué <olivier28.coue@gmail.com>
2016-04-04 12:18:15 +0200

    Create server base + public folder

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+var express         = require('express');
+var app             = express();
+var server          = require('http').Server(app);
+var io              = require('socket.io').listen(server);
+var mongoose 		= require('mongoose');
+
+mongoose.connect('mongodb://localhost/test');
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 8080;
+
+var router = express.Router();              
+
+app.get('/test', function(req, res) {
+   res.json({message: "hello"});
+});
+
+io.sockets.on('connection', function (socket) {
+    console.log('New user connected !');
+    console.log('ID :  '+socket.id);    
+    socket.on('disconnect', function() {
+       
+    });
+});
+
+server.listen(port, function(){
+	console.log("Server running on port: "+port)
+});
