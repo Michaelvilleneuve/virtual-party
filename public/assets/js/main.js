0673d93996c71e3c2ca3c8509b487420056d45cd
Frederic Briolet <fredericbriolet@gmail.com>
2016-04-11 11:42:12 +0200

    Merge again

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,8 +1,5 @@
 var socket = io();
 var pseudo = prompt("Pseudo?");
-<<<<<<< HEAD
-socket.emit("set_pseudo", pseudo);
-=======
 socket.emit("set_pseudo", pseudo);
 socket.on('message', function(message){
 	document.getElementById('messages').innerHTML = document.getElementById('messages').innerHTML + "<br/>" + message.user.pseudo + ":" + message.message;
@@ -11,5 +8,3 @@ function send() {
 	socket.emit("message", document.getElementById("message").value);
 	document.getElementById("message").value = "";
 }
-
->>>>>>> 83ff2f9794d85758367647d262a53c1fe83d8e0f
